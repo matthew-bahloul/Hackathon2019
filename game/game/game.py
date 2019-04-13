45b23c43e979d7fd64c88e3b2b1abbfdfead85f2
Matthew Bahloul <matthew.bahloul@gmail.com>
2019-04-13 08:50:07 -0700

    added a temporary character block, moving, and jumping logic

--- game/game/game.py
+++ game/game/game.py
@@ -3,7 +3,7 @@ import pygame;
 pygame.init() # initialize pygame
 pygame.display.set_caption("Pumpkin Boi") # set the title of the window
 
-size = (650, 500) #set the size of the window
+size = (1500, 900) #set the size of the window
 screen = pygame.display.set_mode(size) # not really sure what this does
 
 done = False # boolean to determine if the game has been exited
@@ -17,7 +17,20 @@ pygame.mixer.music.set_endevent(pygame.constants.USEREVENT)
 pygame.mixer.music.play()
 
 # boolean to trigger game over scree
-game_over = False;
+#game_over = False;
+
+#-------------Create Sprites-----------------------------#
+
+# main guy
+width = 75
+height = 175
+x = 0
+y = 725
+vel = 8
+
+# grunts
+
+# final boss
 
 #--------------Display instruction page------------------#
 
@@ -55,7 +68,7 @@ while not done and display_instructions:
         text = font.render("Intro", True, (255, 255, 255))
         screen.blit(text, [10, 10])
  
-        text = font.render("He will die for his transgressions.", True, (255, 255, 255))
+        text = font.render("He must die for his transgressions.", True, (255, 255, 255))
         screen.blit(text, [10, 40])
     if instruction_page == 3:
         # Draw instructions, page 3
@@ -85,21 +98,65 @@ while not done and display_instructions:
     # Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
 
+    background = pygame.image.load("BG.png")
+
+    screen.blit(background,(0,0)) #place the background  image
+
+
+ 
+# stuf to jump
+isJump = False
+jumpCount = 10
+
+#stuff to slide
+isSlide = False
+
+#stuff for direction
+direction = 1
 
 #--------------Main Program Loop---------------#
-
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
     #----------Game Logic----------------------#
+    keys = pygame.key.get_pressed() #get key inputs
+
+    # moving and jumping logic
+    if keys[pygame.K_LEFT]:
+        direction = -1
+        x -= vel
+    if keys[pygame.K_RIGHT]:
+        direction = 1
+        x += vel
+    if keys[pygame.K_DOWN] and not isSlide:
+            x += direction * 2.5 * vel
+            isSlide = False
+
+    if not (isJump):
+        if keys[pygame.K_UP]:
+            y -= vel
+        
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            neg = 1
+            if jumpCount < 0:
+                neg = -1
+            y -= (jumpCount ** 2) * .5 * neg
+            jumpCount -= 1
+        else:
+            isJump = False
+            jumpCount = 10
 
 
 
-    screen.fill((255,255,255))
-    screen.blit()
+    pygame.draw.rect(screen, (255,0,0), (x,y,width, height))
+    pygame.display.update()
 
+    screen.blit(background,(0,0)) #place the background  image
     # display game over screen
     #if game_over:
     #    # If game over is true, draw game over
