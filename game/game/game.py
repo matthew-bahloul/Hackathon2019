0a390e6caca38d25b3d7a5a854603706d98f1921
Matthew Bahloul <matthew.bahloul@gmail.com>
2019-04-12 23:27:04 -0700

    added some sound files for jumping, rain, hitting

--- game/game/game.py
+++ game/game/game.py
@@ -12,9 +12,9 @@ clock = pygame.time.Clock() # determines how fast the screen updates
 
 
 # Play the background music
-#pygame.mixer.music.load(None) #the name of the music file in here
-#pygame.mixer.music.set_endevent(pygame.constants.USEREVENT)
-#pygame.mixer.music.play()
+pygame.mixer.music.load("bensound-creepy.mp3") #the name of the music file in here
+pygame.mixer.music.set_endevent(pygame.constants.USEREVENT)
+pygame.mixer.music.play()
 
 #--------------Display instruction page------------------#
 
@@ -31,7 +31,7 @@ while not done and display_instructions:
             done = True
         if event.type == pygame.MOUSEBUTTONDOWN:
             instruction_page += 1
-            if instruction_page == 3:
+            if instruction_page == 6:
                 display_instructions = False
  
     # Set the screen background
@@ -42,22 +42,44 @@ while not done and display_instructions:
         # This could also load an image created in another program.
         # That could be both easier and more flexible.
  
-        text = font.render("Instructions", True, (255,255,255))
+        text = font.render("Intro", True, (255,255,255))
         screen.blit(text, [10, 10])
  
-        text = font.render("The big bad boss boi ate your pumpkin seeds.", True, (255,255,255))
+        text = font.render("The big bad boss boy ate your pumpkin seed.", True, (255,255,255))
         screen.blit(text, [10, 40])
  
     if instruction_page == 2:
         # Draw instructions, page 2
-        text = font.render("Instructions", True, (255, 255, 255))
+        text = font.render("Intro", True, (255, 255, 255))
         screen.blit(text, [10, 10])
  
-        text = font.render("He must die for his transgressions.", True, (255, 255, 255))
+        text = font.render("He will die for his transgressions.", True, (255, 255, 255))
         screen.blit(text, [10, 40])
 
-
+    if instruction_page == 3:
+        # Draw instructions, page 3
+        text = font.render("Controls (1)", True, (255, 255, 255))
+        screen.blit(text, [10, 10])
  
+        text = font.render("[left arrow] [right arrow]: move left and right", True, (255, 255, 255))
+        screen.blit(text, [10, 40])
+
+    if instruction_page == 4:
+        # Draw instructions, page 4
+        text = font.render("Controls (2)", True, (255, 255, 255))
+        screen.blit(text, [10, 10])
+ 
+        text = font.render("[space] : jump", True, (255, 255, 255))
+        screen.blit(text, [10, 40])
+
+    if instruction_page == 5:
+        # Draw instructions, page 5
+        text = font.render("Controls (3)", True, (255, 255, 255))
+        screen.blit(text, [10, 10])
+ 
+        text = font.render("[down arrow] : slide", True, (255, 255, 255))
+        screen.blit(text, [10, 40])
+
     # Limit to 60 frames per second
     clock.tick(60)
  
